64977c355aebc24014f1f5f3da11b56ee3db6e82
) Babi_Sunar <babi.sunar3737@gmail.com>
2025-12-01 21:45:11 +0545

    Update guess_the_number.js
    added some alerts to notify player whether the number is larger or smaller

--- Projects/guess_number_game/guess_the_number.js
+++ Projects/guess_number_game/guess_the_number.js
@@ -7,19 +7,25 @@ function generate_random_num(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let random_num = generate_random_num(1, 10);//random value generated
+let random_num = generate_random_num(1, 100);//random value generated
 console.log(random_num);
 let user_input;
 let no_of_guess = [];
 let score;
 
 do {
-    user_input = parseInt(prompt("\t\t<-- GUESS THE CORRECT NUMBER BETWEEN 1 & 10-->"));
+    user_input = parseInt(prompt("\t\t<-- GUESS THE CORRECT NUMBER BETWEEN 1 & 100-->"));
     no_of_guess.push(user_input);
-    if(random_num !== user_input){
-        alert("Wrong choice! please try again.");
+    if(random_num <= 0 || random_num > 100){
+        alert("Invalid input!);
+    }
+    else if(random_num > user_input){
+        alert("Sorry, Try bigger one!");
+    }
+    else if(random_num < user_input){
+        alert("Sorry, Try smaller one!");
     }
 } while (user_input != random_num);
 
-score = 100 - (no_of_guess.length * 10);
+score = 100 - (no_of_guess.length);
 alert(`Congratulations!! Your guess is correct. \n Score : ${score}`);
